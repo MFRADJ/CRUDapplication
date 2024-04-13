891d1f2f7d64531f98de955e356272fecf0afe64
MFRADJ <94693107+MFRADJ@users.noreply.github.com>
2024-04-13 15:49:23 +0200

    Class exception updated (#1)

--- src/main/java/org/javaprojects/crudapplication/exception/RessourceNotFoundException.java
+++ src/main/java/org/javaprojects/crudapplication/exception/RessourceNotFoundException.java
@@ -1,8 +1,23 @@
 package org.javaprojects.crudapplication.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Getter
 public class RessourceNotFoundException extends RuntimeException{
-    private static final long seriaLVersionUID = 1L;
-    public RessourceNotFoundException(String message) {
-        super(message);
+
+
+    final String resourcename;
+    final String filedname;
+    final Object filedvalue;
+
+    public RessourceNotFoundException(String resourceName, String filedName, Object filedValue)
+    {
+        super(String.format("%s not found with %s : '%s'", resourceName, filedName, filedValue ));
+        this.resourcename = resourceName;
+        this.filedname = filedName;
+        this.filedvalue = filedValue;
     }
 }
