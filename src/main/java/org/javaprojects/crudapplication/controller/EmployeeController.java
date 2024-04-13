1852c2f95974b4eb3b5bd230b851b5c91097662f
MFRADJ <94693107+MFRADJ@users.noreply.github.com>
2024-04-13 17:10:45 +0200

    Controller branch (#2)
    * Class exception updated
    
    * Class controller  updated
    
    * Class controller  updated
    
    * Class controller  updated
    
    * Class controller  updated

--- src/main/java/org/javaprojects/crudapplication/controller/EmployeeController.java
+++ src/main/java/org/javaprojects/crudapplication/controller/EmployeeController.java
@@ -1,22 +1,56 @@
 package org.javaprojects.crudapplication.controller;
 
 import org.javaprojects.crudapplication.model.Employee;
-import org.javaprojects.crudapplication.repository.EmployeesRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.javaprojects.crudapplication.service.EmployeesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-    @Autowired
-    private EmployeesRepository employeesRepository;
-    // get all employees
-    @GetMapping("/employees")
+    final EmployeesService employeesService;
+
+    public EmployeeController(EmployeesService employeesService) {
+        super();
+        this.employeesService = employeesService;
+    }
+
+    //build create employee REST API
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeesService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    // build get all employees REST API
+    @GetMapping()
     public List<Employee> getAllEmployees() {
-        return employeesRepository.findAll();
+        return employeesService.getAllEmployees();
+    }
+
+    // build get employee by id REST API
+    // http://localhost:8080/api/employees/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+        Employee employee = employeesService.getEmployeeById(employeeId);
+        return ResponseEntity.ok().body(employee);
+    }
+
+    // build update employee by id REST API
+    // http://localhost:8080/api/employees/1
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") long employeeId, @RequestBody Employee employeeDetails) {
+        Employee updatedEmployee = employeesService.updateEmployee(employeeDetails, employeeId);
+        return ResponseEntity.ok().body(updatedEmployee);
+    }
+
+    // build delete employee by id REST API
+    // http://localhost:8080/api/employees/1
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable("id") long employeeId) {
+        employeesService.deleteEmployee(employeeId);
+        return ResponseEntity.noContent().build();
     }
 }
