f89beb711b421654189a043cbe724fa09f3ecc98
MFRADJ <mohamed-fradj@outlook.com>
2024-04-13 16:00:43 +0200

    Class controller  updated

--- src/main/java/org/javaprojects/crudapplication/controller/EmployeeController.java
+++ src/main/java/org/javaprojects/crudapplication/controller/EmployeeController.java
@@ -2,21 +2,29 @@ package org.javaprojects.crudapplication.controller;
 
 import org.javaprojects.crudapplication.model.Employee;
 import org.javaprojects.crudapplication.repository.EmployeesRepository;
+import org.javaprojects.crudapplication.service.EmployeesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-    @Autowired
-    private EmployeesRepository employeesRepository;
-    // get all employees
-    @GetMapping("/employees")
-    public List<Employee> getAllEmployees() {
-        return employeesRepository.findAll();
+   private EmployeesService employeesService;
+
+    public EmployeeController(EmployeesService employeesService) {
+        super();
+        this.employeesService = employeesService;
     }
+    //build create employee REST API
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<Employee>(employeesService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+    // build get all emplo
+
 }
